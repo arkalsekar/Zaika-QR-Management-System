2443ae39e2d702587ffc2e8667b0193389dee31f
arkalsekar <abdulrehmankalsekar10@gmail.com>
2026-02-04 11:39:29 +0530

    Added ENV

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vskqcbkzggkrzahdnlre.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZza3FjYmt6Z2drcnphaGRubHJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTgyOTksImV4cCI6MjA4MzQ3NDI5OX0.kJFRwcp0j5a4-KWLBDMnbDxaxBLHQ__K1d8aLiVao34'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
